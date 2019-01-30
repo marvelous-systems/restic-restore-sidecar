6ae8591b8bca2ef64813ce7f35f0dd653c85ab99
Noah Hummel <n.p.hummel@gmail.com>
2019-01-30 04:49:07 +0100

    [FIX] remove path from snapshots to restore

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 echo "Starting restore..."
-for path_and_hash in $(echo $RESTORE_PATHS | sed "s/,/ /g")
+for hash in $(echo $RESTORE_SNAPSHOTS | sed "s/,/ /g")
 do
-	path=$(echo "$path_and_hash" | cut -d':' -f1)
-	snapshot_hash=$(echo "$path_and_hash" | cut -d':' -f2)
 	./restic \
 		-r "sftp::${SFTP_PATH}" \
 		-o sftp.command="ssh ${SFTP_USER}@${SFTP_HOST} -p ${SFTP_PORT} -o StrictHostKeyChecking=no -i /restic/key -s sftp" \
 		--verbose \
-		restore "$snapshot_hash" \
-		--target "$path"
+		restore "$hash" \
+		--target /
 done
